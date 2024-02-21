54286485a8f7be6723d93aacc2a83da7d6f04112
ShreyasKyada <kyadashreyas@gmail.com>
2024-02-22 00:21:46 +0530

    first commit

---
+++ consts.js
@@ -0,0 +1,49 @@
+export const NPM_TYPESCRIPT = "npm install typescript --save-dev"
+
+export const EXPRESS_INSTALL_COMMANDS = {
+  npm: "npm install express",
+  pnpm: "pnpm install express",
+  yarn: "yarn add express",
+}
+
+export const TYPESCRIPT_INSTALL_COMMANDS = {
+  npm: "npm install typescript",
+  pnpm: "pnpm install typescript",
+  yarn: "yarn add typescript",
+}
+
+export const TYPES_INSTALL_COMMAND = {
+  npm: "npm install @types/express @types/node --save-dev",
+  pnpm: "pnpm install @types/express @types/node --save-dev",
+  yarn: "yarn add @types/express @types/node --dev",
+}
+
+export const ESLINT_INSTALL_COMMAND = {
+  npm: "npm install @typescript-eslint/eslint-plugin@^7.0.1 @typescript-eslint/parser@^7.0.1 eslint@^8.56.0 --save-dev",
+  pnpm: "pnpm install @typescript-eslint/eslint-plugin@^7.0.1 @typescript-eslint/parser@^7.0.1 eslint@^8.56.0 --save-dev",
+  yarn: "yarn add @typescript-eslint/eslint-plugin@^7.0.1 @typescript-eslint/parser@^7.0.1 eslint@^8.56.0 --dev",
+}
+
+export const PRETTIER_INSTALL_COMMAND = {
+  npm: "npm install eslint-config-prettier@^9.1.0 eslint-plugin-prettier@^5.1.3 --save-dev",
+  pnpm: "pnpm install eslint-config-prettier@^9.1.0 eslint-plugin-prettier@^5.1.3 --save-dev",
+  yarn: "yarn add eslint-config-prettier@^9.1.0 eslint-plugin-prettier@^5.1.3 --dev",
+}
+
+export const getDefaultProjectJSON = (name) => ({
+  name: name,
+  version: "1.0.0",
+  description: "",
+  main: "src/index.ts",
+  type: "module",
+  scripts: {
+    build: "npx tsc",
+    preserver: "npm run build",
+    server: 'npx concurrently "npx tsc -w"  "nodemon dist/server.js"',
+  },
+  keywords: [],
+  author: "",
+  license: "ISC",
+  dependencies: {},
+  devDependencies: {},
+})
---
+++ index.js
@@ -0,0 +1,99 @@
+#!/usr/bin/env node
+
+import { program } from "commander"
+import path, { dirname } from "path"
+import fs from "fs"
+import { execSync } from "child_process"
+import inquirer from "inquirer"
+import { fileURLToPath } from "url"
+import {
+  ESLINT_INSTALL_COMMAND,
+  EXPRESS_INSTALL_COMMANDS,
+  PRETTIER_INSTALL_COMMAND,
+  TYPESCRIPT_INSTALL_COMMANDS,
+  TYPES_INSTALL_COMMAND,
+  getDefaultProjectJSON,
+} from "./consts.js"
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+program.option("--no-ts", "setup typescript")
+program.option("--no-eslint", "setup eslint")
+program.option("--no-prettier", "setup prettier")
+const options = program.opts()
+
+program.parse()
+
+const prompt = inquirer.createPromptModule()
+let dirName = process.argv[2]
+
+if (!dirName) {
+  prompt([
+    {
+      name: "project_name",
+      type: "input",
+      default: "express-ts",
+      message: "Enter project name:",
+    },
+  ]).then((ans) => {
+    console.log("Project name:", ans.project_name)
+    dirName = ans.project_name
+    askPackageManager()
+  })
+} else {
+  askPackageManager()
+}
+
+function askPackageManager() {
+  prompt([
+    {
+      name: "package_manager",
+      message: "Select package manager:",
+      type: "list",
+      choices: ["npm", "pnpm", "yarn"],
+    },
+  ]).then(({ package_manager }) => {
+    console.log("Creating directory:")
+    const projectDir = path.basename(__dirname)
+
+    let baseCommand = ""
+    if (dirName !== ".") {
+      fs.mkdirSync(dirName)
+
+      let baseCommand = `cd ${dirName} && `
+    }
+
+    const projectJSON = getDefaultProjectJSON(dirName === "." ? projectDir : dirName)
+
+    console.log("initializing project...")
+    fs.cpSync(path.resolve(__dirname, "src/template"), dirName, {
+      recursive: true,
+    })
+
+    console.log("Generating package.json...")
+    fs.writeFileSync(dirName + "/package.json", JSON.stringify(projectJSON, null, 2))
+
+    console.log("Installing expressJS...")
+    execSync(baseCommand + EXPRESS_INSTALL_COMMANDS[package_manager])
+
+    if (!options["no-ts"]) {
+      console.log("Installing TypeScript...")
+      execSync(baseCommand + TYPESCRIPT_INSTALL_COMMANDS[package_manager])
+      execSync(baseCommand + TYPES_INSTALL_COMMAND[package_manager])
+      fs.copyFileSync(path.resolve(__dirname, "src/tsconfig.json"), dirName + "/tsconfig.json")
+    }
+
+    if (!options["no-eslint"]) {
+      console.log("Installing ESLint...")
+      execSync(baseCommand + ESLINT_INSTALL_COMMAND[package_manager])
+      fs.copyFileSync(path.resolve(__dirname, "src/.eslintrc.cjs"), dirName + "/.eslintrc.cjs")
+    }
+
+    if (!options["no-prettier"]) {
+      console.log("Installing prettier...")
+      execSync(baseCommand + PRETTIER_INSTALL_COMMAND[package_manager])
+      fs.copyFileSync(path.resolve(__dirname, "src/.prettierrc"), dirName + "/.prettierrc")
+    }
+  })
+}
